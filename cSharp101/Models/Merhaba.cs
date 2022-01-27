48b0a02596892c49f3d0ca32b47f3596d06c7ca2
Sefika Nur Topuz <sefikanurtopuz@gmail.com>
2022-01-27 13:09:58 +0300

    first commit

---
+++ cSharp101/Models/Merhaba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp101.Models
+{
+    public class Merhaba
+    {
+        public void helloWorld()
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("İsim: ");
+            string isim= Console.ReadLine();
+
+            Console.WriteLine("Soyisim: ");
+            string soyisim= Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + isim + " " + soyisim);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ cSharp101/Program.cs
@@ -0,0 +1,23 @@
+﻿using cSharp101.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Patika.dev
+            // C# 101
+
+            //Main Methodu - Namespace kavramı - System Using Direktifleri - Run dersi ödevi
+            Merhaba merhaba = new Merhaba();
+            merhaba.helloWorld();
+
+        }
+    }
+}
